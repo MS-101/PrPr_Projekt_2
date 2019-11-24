194db8f18943e2c87e0e8cd3808288807db3d8e4
Martin Svab <martin.svab@gmail.com>
2019-11-24 12:15:05 +0100

    pridane funkcie nacitajZoznam a vypisZoznam

--- Projekt2.c
+++ Projekt2.c
@@ -3,45 +3,137 @@
 #include <string.h>
 
 typedef struct zaznam {
-    char[50] kategoriaPonuky;
-    char[50] miestoPonuky;
-    char[100] ulica;
+    char kategoriaPonuky[50];
+    char miestoPonuky[50];
+    char ulica[100];
     int rozloha;
     int cena;
-    char[200] popis;
-} ZAZNAM
+    char popis[200];
+    struct zaznam *dalsi;
+} ZAZNAM;
 
-void nacitajZaznam() {
+void vycistiBuffer(FILE **fr) {
+    while (fgetc(*fr) != '\n');
+}
+
+void odstranEnter(char *buf) {
+    buf[strlen(buf) - 1] = '\0';
+}
+
+void nacitajZaznam(ZAZNAM **prvyZaznam) {
+    FILE *fr;
+    ZAZNAM *aktualnyZaznam, *novyZaznam;
+    aktualnyZaznam = *prvyZaznam;
+
+    if ((fr = fopen("reality.txt", "r")) == NULL) {
+        printf("Zaznamy neboli nacitane\n");
+        return;
+    }
+
+    if (*prvyZaznam != NULL) {
+        while (aktualnyZaznam != NULL) {
+            *prvyZaznam = aktualnyZaznam -> dalsi;
+            free(aktualnyZaznam);
+            aktualnyZaznam = *prvyZaznam;
+        }
+    }
+
+    int pocetZaznamov = 0;
+    char kategoriaPonuky[50];
+    char miestoPonuky[50];
+    char ulica[100];
+    int rozloha;
+    int cena;
+    char popis[200];
+
+    while (fgetc(fr) == '&') {
+        pocetZaznamov++;
+
+        //nacitanie udajov zo suboru
+        vycistiBuffer(&fr);
+        fgets(kategoriaPonuky, 50, fr);
+        odstranEnter(kategoriaPonuky);
+        fgets(miestoPonuky, 50, fr);
+        odstranEnter(miestoPonuky);
+        fgets(ulica, 100, fr);
+        odstranEnter(ulica);
+        fscanf(fr, "%d\n", &rozloha);
+        fscanf(fr, "%d\n", &cena);
+        fgets(popis, 200, fr);
+        odstranEnter(popis);
+
+        //zapis udajov do spajaneho zoznamu
+        if (pocetZaznamov == 1) {
+            *prvyZaznam = (ZAZNAM *)malloc(sizeof(ZAZNAM));
+            strcpy((*prvyZaznam) -> kategoriaPonuky, kategoriaPonuky);
+            strcpy((*prvyZaznam) -> miestoPonuky, miestoPonuky);
+            strcpy((*prvyZaznam) -> ulica, ulica);
+            (*prvyZaznam) -> rozloha = rozloha;
+            (*prvyZaznam) -> cena = cena;
+            strcpy((*prvyZaznam) -> popis, popis);
+            aktualnyZaznam = *prvyZaznam;
+        } else {
+            aktualnyZaznam -> dalsi = (ZAZNAM *)malloc(sizeof(ZAZNAM));
+            aktualnyZaznam = aktualnyZaznam -> dalsi;
+            strcpy(aktualnyZaznam -> kategoriaPonuky, kategoriaPonuky);
+            strcpy(aktualnyZaznam -> miestoPonuky, miestoPonuky);
+            strcpy(aktualnyZaznam -> ulica, ulica);
+            aktualnyZaznam -> rozloha = rozloha;
+            aktualnyZaznam -> cena = cena;
+            strcpy(aktualnyZaznam -> popis, popis);
+        }
+    }
+
+    if (pocetZaznamov > 0) {
+        aktualnyZaznam -> dalsi = NULL;
+    }
+
+    printf("Nacitalo sa %d zaznamov\n", pocetZaznamov);
+    fclose(fr);
+}
+
+void vypisZaznam(ZAZNAM *prvyZaznam) {
+    ZAZNAM *aktualnyZaznam = prvyZaznam;
+    int cisloZaznamu = 1;
+    while (aktualnyZaznam != NULL) {
+        printf("%d.\n", cisloZaznamu);
+        printf("kategoria ponuky: %s\n", aktualnyZaznam -> kategoriaPonuky);
+        printf("miesto ponuky: %s\n", aktualnyZaznam -> miestoPonuky);
+        printf("ulica: %s\n", aktualnyZaznam -> ulica);
+        printf("rozloha v m2: %d\n", aktualnyZaznam -> rozloha);
+        printf("cena: %d\n", aktualnyZaznam -> cena);
+        printf("popis: %s\n", aktualnyZaznam -> popis);
+        aktualnyZaznam = aktualnyZaznam -> dalsi;
+        cisloZaznamu++;
+    }
+}
+
+void pridajZaznam(ZAZNAM **prvyZaznam) {
 
 }
 
-void vypisZaznam() {
+void zmazZaznamy(ZAZNAM **prvyZaznam) {
 
 }
 
-void pridajZaznam() {
+void vyhladajZaznamy(ZAZNAM *prvyZaznam) {
 
 }
 
-void zmazZaznamy() {
+void aktualizujZaznamy(ZAZNAM **prvyZaznam) {
 
 }
 
-void aktualizujZaznamy() {
-
-}
-
-int ukoncProgram {
+int ukoncProgram() {
     return 0;
 }
 
 int main() {
-    FILE *fr = NULL;
     ZAZNAM *prvyZaznam = NULL;
     while (1)  {
         switch (getchar()) {
             case 'n':
-                nacitajZaznam(&fr, &prvyZaznam);
+                nacitajZaznam(&prvyZaznam);
                 break;
             case 'v':
                 vypisZaznam(prvyZaznam);

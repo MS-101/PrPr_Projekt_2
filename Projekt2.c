a35d6b5e3b164d2dee6a3f905b6e21100df249bf
Martin Svab <martin.svab@gmail.com>
2019-11-24 19:52:38 +0100

    pridana funkcia hladajZaznamy a aktualizujZaznamy

--- Projekt2.c
+++ Projekt2.c
@@ -12,12 +12,35 @@ typedef struct zaznam {
     struct zaznam *dalsi;
 } ZAZNAM;
 
-void vycistiBuffer(FILE **fr) {
+void vycistiBufferSuboru(FILE **fr) {
     while (fgetc(*fr) != '\n');
 }
 
-void odstranEnter(char *buf) {
+void vycistiBufferVstupu() {
+    while (getchar() != '\n');
+}
+
+
+void odstranNewLine(char *buf) {
     buf[strlen(buf) - 1] = '\0';
+}
+
+void vymazZoznam(ZAZNAM ***prvyZaznam) {
+    ZAZNAM *aktualnyZaznam;
+    aktualnyZaznam = **prvyZaznam;
+    if (**prvyZaznam != NULL) {
+        while (aktualnyZaznam != NULL) {
+            **prvyZaznam = aktualnyZaznam -> dalsi;
+            free(aktualnyZaznam);
+            aktualnyZaznam = **prvyZaznam;
+        }
+    }
+}
+
+void strLow(char *buf) {
+    for (int i = 0; i < strlen(buf); i++) {
+        buf[i] = tolower(buf[i]);
+    }
 }
 
 void nacitajZaznam(ZAZNAM **prvyZaznam) {
@@ -30,13 +53,7 @@ void nacitajZaznam(ZAZNAM **prvyZaznam) {
         return;
     }
 
-    if (*prvyZaznam != NULL) {
-        while (aktualnyZaznam != NULL) {
-            *prvyZaznam = aktualnyZaznam -> dalsi;
-            free(aktualnyZaznam);
-            aktualnyZaznam = *prvyZaznam;
-        }
-    }
+    vymazZoznam(&prvyZaznam);
 
     int pocetZaznamov = 0;
     char kategoriaPonuky[50];
@@ -50,17 +67,17 @@ void nacitajZaznam(ZAZNAM **prvyZaznam) {
         pocetZaznamov++;
 
         //nacitanie udajov zo suboru
-        vycistiBuffer(&fr);
+        vycistiBufferSuboru(&fr);
         fgets(kategoriaPonuky, 50, fr);
-        odstranEnter(kategoriaPonuky);
+        odstranNewLine(kategoriaPonuky);
         fgets(miestoPonuky, 50, fr);
-        odstranEnter(miestoPonuky);
+        odstranNewLine(miestoPonuky);
         fgets(ulica, 100, fr);
-        odstranEnter(ulica);
+        odstranNewLine(ulica);
         fscanf(fr, "%d\n", &rozloha);
         fscanf(fr, "%d\n", &cena);
         fgets(popis, 200, fr);
-        odstranEnter(popis);
+        odstranNewLine(popis);
 
         //zapis udajov do spajaneho zoznamu
         if (pocetZaznamov == 1) {
@@ -92,19 +109,23 @@ void nacitajZaznam(ZAZNAM **prvyZaznam) {
     fclose(fr);
 }
 
-void vypisZaznam(ZAZNAM *prvyZaznam) {
+void vypisZaznam(int cisloZaznamu, ZAZNAM *aktualnyZaznam) {
+    printf("%d.\n", cisloZaznamu);
+    printf("kategoria ponuky: %s\n", aktualnyZaznam -> kategoriaPonuky);
+    printf("miesto ponuky: %s\n", aktualnyZaznam -> miestoPonuky);
+    printf("ulica: %s\n", aktualnyZaznam -> ulica);
+    printf("rozloha v m2: %d\n", aktualnyZaznam -> rozloha);
+    printf("cena: %d\n", aktualnyZaznam -> cena);
+    printf("popis: %s\n", aktualnyZaznam -> popis);
+}
+
+void vypisZoznam(ZAZNAM *prvyZaznam) {
     ZAZNAM *aktualnyZaznam = prvyZaznam;
-    int cisloZaznamu = 1;
+    int cisloZaznamu = 0;
     while (aktualnyZaznam != NULL) {
-        printf("%d.\n", cisloZaznamu);
-        printf("kategoria ponuky: %s\n", aktualnyZaznam -> kategoriaPonuky);
-        printf("miesto ponuky: %s\n", aktualnyZaznam -> miestoPonuky);
-        printf("ulica: %s\n", aktualnyZaznam -> ulica);
-        printf("rozloha v m2: %d\n", aktualnyZaznam -> rozloha);
-        printf("cena: %d\n", aktualnyZaznam -> cena);
-        printf("popis: %s\n", aktualnyZaznam -> popis);
-        aktualnyZaznam = aktualnyZaznam -> dalsi;
         cisloZaznamu++;
+        vypisZaznam(cisloZaznamu, aktualnyZaznam);
+        aktualnyZaznam = aktualnyZaznam -> dalsi;
     }
 }
 
@@ -122,18 +143,18 @@ void pridajZaznam(ZAZNAM **prvyZaznam) {
     char popis[200];
 
     //nacitanie hodnot noveho zaznamu
-    while (getchar() != '\n');
+    vycistiBufferVstupu();
     fgets(kategoriaPonuky, 50, stdin);
-    odstranEnter(kategoriaPonuky);
+    odstranNewLine(kategoriaPonuky);
     fgets(miestoPonuky, 50, stdin);
-    odstranEnter(miestoPonuky);
+    odstranNewLine(miestoPonuky);
     fgets(ulica, 100, stdin);
-    odstranEnter(ulica);
+    odstranNewLine(ulica);
     scanf("%d", &rozloha);
     scanf("%d", &cena);
-    while (getchar() != '\n');
+    vycistiBufferVstupu();
     fgets(popis, 200, stdin);
-    odstranEnter(popis);
+    odstranNewLine(popis);
 
     //zapis hodnot noveho zaznamu
     novyZaznam = (ZAZNAM*)malloc(sizeof(ZAZNAM));
@@ -166,25 +187,21 @@ void zmazZaznamy(ZAZNAM **prvyZaznam) {
     ZAZNAM *aktualnyZaznam, *predoslyZaznam, *odstranenyZaznam;
     char hladaneMiesto[50];
     char najdeneMiesto[50];
-    int i;
     int pocetZmazanych = 0;
 
-    scanf("%s", hladaneMiesto);
-    for (i = 0; i < strlen(hladaneMiesto); i++) {
-        hladaneMiesto[i] = tolower(hladaneMiesto[i]);
-    }
+    vycistiBufferVstupu();
+    fgets(hladaneMiesto, 50, stdin);
+    odstranNewLine(hladaneMiesto);
+    strLow(hladaneMiesto);
 
     aktualnyZaznam = *prvyZaznam;
     while (aktualnyZaznam != NULL) {
         strcpy(najdeneMiesto, aktualnyZaznam -> miestoPonuky);
-        for (i = 0; i < strlen(najdeneMiesto); i++) {
-            najdeneMiesto[i] = tolower(najdeneMiesto[i]);
-        }
+        strLow(najdeneMiesto);
 
         if (strstr(najdeneMiesto, hladaneMiesto) != NULL) {
             pocetZmazanych++;
             odstranenyZaznam = aktualnyZaznam;
-            printf("Bol odstraneny zaznam s miestom: %s\n", odstranenyZaznam -> miestoPonuky);
             if (aktualnyZaznam == *prvyZaznam) {
                 *prvyZaznam = (*prvyZaznam) -> dalsi;
             } else {
@@ -201,23 +218,78 @@ void zmazZaznamy(ZAZNAM **prvyZaznam) {
     printf("Vymazalo sa %d zaznamov\n", pocetZmazanych);
 }
 
-void vyhladajZaznamy(ZAZNAM *prvyZaznam) {
+void hladajZaznamy(ZAZNAM *prvyZaznam) {
+    int pocetZaznamov = 0, hladanaCena;
+    ZAZNAM *aktualnyZaznam;
+    scanf("%d", &hladanaCena);
 
+    aktualnyZaznam = prvyZaznam;
+    while (aktualnyZaznam != NULL) {
+        if (aktualnyZaznam -> cena <= hladanaCena) {
+            pocetZaznamov++;
+            vypisZaznam(pocetZaznamov, aktualnyZaznam);
+        }
+        aktualnyZaznam = aktualnyZaznam -> dalsi;
+    }
+
+    if (pocetZaznamov == 0) {
+        printf("V ponuke su len reality s vyssou cenou\n");
+    }
 }
 
 void aktualizujZaznamy(ZAZNAM **prvyZaznam) {
+    char hladaneMiesto[50], najdeneMiesto[50];
+    ZAZNAM *aktualnyZaznam;
 
+    vycistiBufferVstupu();
+    fgets(hladaneMiesto, 50, stdin);
+    odstranNewLine(hladaneMiesto);
+    strLow(hladaneMiesto);
+
+    int pocetAktualizovanych = 0;
+    char kategoriaPonuky[50];
+    char miestoPonuky[50];
+    char ulica[100];
+    int rozloha;
+    int cena;
+    char popis[200];
+
+    //nacitanie hodnot aktualneho zaznamu
+    fgets(kategoriaPonuky, 50, stdin);
+    odstranNewLine(kategoriaPonuky);
+    fgets(miestoPonuky, 50, stdin);
+    odstranNewLine(miestoPonuky);
+    fgets(ulica, 100, stdin);
+    odstranNewLine(ulica);
+    scanf("%d", &rozloha);
+    scanf("%d", &cena);
+    vycistiBufferVstupu();
+    fgets(popis, 200, stdin);
+    odstranNewLine(popis);
+
+    aktualnyZaznam = *prvyZaznam;
+    while (aktualnyZaznam != NULL) {
+        strcpy(najdeneMiesto, aktualnyZaznam -> miestoPonuky);
+        strLow(najdeneMiesto);
+
+        if (strstr(najdeneMiesto, hladaneMiesto) != NULL) {
+            pocetAktualizovanych++;
+            strcpy(aktualnyZaznam -> kategoriaPonuky, kategoriaPonuky);
+            strcpy(aktualnyZaznam -> miestoPonuky, miestoPonuky);
+            strcpy(aktualnyZaznam -> ulica, ulica);
+            aktualnyZaznam -> rozloha = rozloha;
+            aktualnyZaznam -> cena = cena;
+            strcpy(aktualnyZaznam -> popis, popis);
+        }
+
+        aktualnyZaznam = aktualnyZaznam -> dalsi;
+    }
+
+    printf("Aktualizovalo sa %d zaznamov\n", pocetAktualizovanych);
 }
 
-int ukoncProgram(ZAZNAM **prvyZaznam) {
-    ZAZNAM *aktualnyZaznam;
-    if (*prvyZaznam != NULL) {
-        while (aktualnyZaznam != NULL) {
-            *prvyZaznam = aktualnyZaznam -> dalsi;
-            free(aktualnyZaznam);
-            aktualnyZaznam = *prvyZaznam;
-        }
-    }
+int koniecProgramu(ZAZNAM **prvyZaznam) {
+    vymazZoznam(&prvyZaznam);
     return 0;
 }
 
@@ -229,7 +301,7 @@ int main() {
                 nacitajZaznam(&prvyZaznam);
                 break;
             case 'v':
-                vypisZaznam(prvyZaznam);
+                vypisZoznam(prvyZaznam);
                 break;
             case 'p':
                 pridajZaznam(&prvyZaznam);
@@ -238,13 +310,13 @@ int main() {
                 zmazZaznamy(&prvyZaznam); //podla miesta ponuky
                 break;
             case 'h':
-                vyhladajZaznamy(prvyZaznam); //podla ceny
+                hladajZaznamy(prvyZaznam); //podla ceny
                 break;
             case 'a':
                 aktualizujZaznamy(&prvyZaznam); //podla miesta ponuky
                 break;
             case 'k':
-                return ukoncProgram(&prvyZaznam);
+                return koniecProgramu(&prvyZaznam);
                 break;
         }
     }
